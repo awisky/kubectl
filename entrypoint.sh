651119d18acbb38f23ffb2da3d301a6be192ef72
Agustin Wisky <agustin@bloopark.de>
2021-07-16 09:06:11 -0300

    [IMP] AWS IAM authenticator binary added

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,6 +16,11 @@ curl -sLO "https://dl.k8s.io/release/$version/bin/linux/amd64/kubectl" -o kubect
 chmod +x kubectl
 mv kubectl /usr/local/bin
 
+echo "using aws-iam-authenticator"
+curl -o aws-iam-authenticator https://amazon-eks.s3.us-west-2.amazonaws.com/1.19.6/2021-01-05/bin/linux/amd64/aws-iam-authenticator
+chmod +x ./aws-iam-authenticator
+mv aws-iam-authenticator /usr/local/bin
+
 # Extract the base64 encoded config data and write this to the KUBECONFIG
 echo "$config" | base64 -d > /tmp/config
 export KUBECONFIG=/tmp/config
